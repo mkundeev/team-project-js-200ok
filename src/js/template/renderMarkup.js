188ef24075001aa729bea1cd30d15676b5c4e85e
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-06-17 13:38:26 +0300

    add request for trending films and render markup

---
+++ src/js/template/renderMarkup.js
@@ -0,0 +1,39 @@
+import { refs } from '../service/refs';
+const { MovieTrendContainer } = refs;
+
+export function renderMovieGallery(results) {
+  MovieTrendContainer.innerHTML = '';
+
+  const markup = results
+    .map(
+      ({ title, vote_average, release_date, genre_ids, poster_path }) =>
+        `
+<li class="gallery-items films__gallery-item">
+<a href="#!" class="list-card__link">
+<!-- постер -->
+<div class="moviе-item__img-container">
+<img
+        src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${poster_path}"
+        alt="${title}"
+        class="moviе-item__img"
+    />
+    </div>
+<!-- обгортка інформації під постером -->
+<div class="moviе-stats">
+    <h2 class="moviе-stats__title">${title}</h2>
+    <div class="moviе-stats__info">
+<!-- список жанрів -->
+<p class="moviе-genre">${genre_ids}</p>
+<!-- дата виходу та рейтинг -->
+<p class="moviе-year">${new Date(release_date).getFullYear()}</p>
+<!-- рейтинг -->
+<p class="moviе-vote">${vote_average}</p>
+</div>
+    </div>
+</a>
+</li>`
+    )
+    .join('');
+
+  MovieTrendContainer.insertAdjacentHTML('beforeend', markup);
+}
