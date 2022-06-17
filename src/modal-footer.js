e5f1d3d61f3b2106c17918d649b252c14bf70ca2
Iuliia <iuliiaiavorskaia@gmail.com>
2022-06-17 17:49:23 +0300

    add fixed  fon

--- src/modal-footer.js
+++ src/modal-footer.js
@@ -4,9 +4,29 @@ const refs = {
   modal: document.querySelector('[data-modal]'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', showModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
 
-function toggleModal() {
+function showModal() {
   refs.modal.classList.toggle('is-hidden');
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
 }
+
+function closeModal() {
+  refs.modal.classList.toggle('is-hidden');
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+}
+
+// window.addEventListener('scroll', () => {
+//   document.documentElement.style.setProperty(
+//     '--scroll-y',
+//     `${window.scrollY}px`
+//   );
+// });
