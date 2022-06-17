3714c6e817a8a1fb24c58eed31294cf518093f31
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-06-17 18:11:18 +0300

    add search and render movies

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-
 import { refs } from './js/service/refs';
 import { MovieService } from './js/service/fetchItems';
-import { renderMovieGallery } from './js/template/renderMarkup';
+import {
+  renderMovieGallery,
+  renderSearchResultMovie,
+} from './js/template/renderMarkup';
 
-// MovieService.getMovieTrend().then(response => console.log(response));
+// MovieService.getSearchMovieResult().then(response => console.log(response));
 // MovieService.getGenres().then(response => console.log(response));
 
-const moviePagination = async event => {
+// запрос и отрисовка популярных фильмов
+const movieTrending = async () => {
   try {
-    refs.MovieTrendContainer.innerHTML = '';
+    refs.movieContainer.innerHTML = '';
 
     const { results } = await MovieService.getMovieTrend();
     renderMovieGallery(results);
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 };
 
-document.addEventListener('DOMContentLoaded', moviePagination);
+document.addEventListener('DOMContentLoaded', movieTrending);
 
+// запрос и отрисовка фильмов по поиску
+const movieSearch = async ev => {
+  ev.preventDefault();
+
+  MovieService._query = ev.target.elements.query.value.trim();
+
+  if (!MovieService._query) return;
+
+  try {
+    refs.movieContainer.innerHTML = '';
+    const { results } = await MovieService.getSearchMovieResult();
+
+    renderSearchResultMovie(results);
+    refs.form.reset();
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+refs.form.addEventListener('submit', movieSearch);
