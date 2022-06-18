39134d1ec3f317d4a2e34d59a913b74c4ef1b5ab
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-06-18 08:15:56 +0300

    add madal open

--- src/js/modal-regist/registration.js
+++ src/js/modal-regist/registration.js
@@ -3,9 +3,6 @@ const refs = {
   btnRegistration: document.querySelector('.regist-btn'),
   formAutorization: document.querySelector('.form-authorization'),
   formRegistration: document.querySelector('.form-register'),
-  closeModalBtnAuth: document.querySelector('.js-btn-modal'),
-  openModalAuth: document.querySelector('.js-site-nav'),
-  modalAuth: document.querySelector('.js-auth-modal'),
 };
 
 refs.btnAutorization.addEventListener('click', onAutorization);
@@ -23,41 +20,5 @@ function onRegistration() {
   refs.btnAutorization.classList.remove('is-active-btn');
   refs.btnRegistration.classList.add('is-active-btn');
 }
-
+controlModal('.js-site-nav', '.js-btn-modal', '.js-auth-modal');
 //відкриття модалки
-
-refs.closeModalBtnAuth.addEventListener('click', toggleModalClose);
-refs.openModalAuth.addEventListener('click', toggleModalOpen);
-refs.modalAuth.addEventListener('click', onClickBackdrop);
-
-function toggleModalClose(e) {
-  e.preventDefault();
-
-  closeModal();
-  window.addEventListener('keydown', onEskClose); //виклик слухача клавіатури
-
-  function onEskClose(event) {
-    if (event.code === 'Escape') {
-      closeModal();
-    }
-
-    window.removeEventListener('keydown', onEskClose); //зняття слухача з клавіатури
-  }
-}
-function onClickBackdrop(event) {
-  if (event.target === event.currentTarget) {
-    closeModal();
-  }
-}
-
-function toggleModalOpen(e) {
-  e.preventDefault();
-  opemModal();
-}
-
-function closeModal() {
-  refs.modalAuth.classList.add('visually-hidden');
-}
-function opemModal() {
-  refs.modalAuth.classList.remove('visually-hidden');
-}
