9a1847a7ae5e187961243a0904c8f88313ca3dce
Iuliia <iuliiaiavorskaia@gmail.com>
2022-06-21 21:42:54 +0300

    fix

--- src/js/day-night/day-night.js
+++ src/js/day-night/day-night.js
@@ -4,12 +4,20 @@ const { toggle } = refs;
 const initialState = localStorage.getItem('toggleState') == 'true';
 toggle.checked = initialState;
 
-toggle.addEventListener('change', function (evt) {
+toggle.addEventListener('change', addNightTheme);
+
+function addNightTheme(evt) {
   evt.preventDefault();
   localStorage.setItem('toggleState', toggle.checked);
-  if (toggle.checked) {
+  const settings = JSON.parse(localStorage.getItem('toggleState'));
+
+  if (settings) {
     document.body.classList.add('night-theme');
+
+    // console.log(settings);
   } else {
     document.body.classList.remove('night-theme');
+
+    // console.log(settings);
   }
-});
+}
