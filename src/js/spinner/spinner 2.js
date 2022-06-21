c3812d1e8ed7a6a6f516f4d3a00cdcbe3f16a867
Maksym Kundeev <mkundeev@gmail.com>
2022-06-21 12:41:54 +0300

    1

--- src/js/spinner/spinner 2.js
+++ src/js/spinner/spinner 2.js
@@ -1,18 +1,18 @@
-export class VisibleComponent {
-  constructor({ selector, className, isHide = false }) {
-    this.selector = document.querySelector(selector);
-    this.className = className;
+// export class VisibleComponent {
+//   constructor({ selector, className, isHide = false }) {
+//     this.selector = document.querySelector(selector);
+//     this.className = className;
 
-    if (isHide) {
-      this.hide();
-    }
-  }
+//     if (isHide) {
+//       this.hide();
+//     }
+//   }
 
-  hide() {
-    this.selector.classList.add(this.className);
-  }
+//   hide() {
+//     this.selector.classList.add(this.className);
+//   }
 
-  show() {
-    this.selector.classList.remove(this.className);
-  }
-}
+//   show() {
+//     this.selector.classList.remove(this.className);
+//   }
+// }
