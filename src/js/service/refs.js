c2ad93da35ac8e4448bff44458bc7ed2016599f6
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-06-21 16:58:00 +0300

    refs modal-regist

--- src/js/service/refs.js
+++ src/js/service/refs.js
@@ -3,4 +3,9 @@ export const refs = {
   movieContainer: document.querySelector('.films__gallery'),
   movieOneCardContainer: document.querySelector('.films__gallery'),
   cardModalMovie: document.querySelector('.js-modal-card'), //на один фильм
+  btnAutorization: document.querySelector('.auth-btn'),
+  btnRegistration: document.querySelector('.regist-btn'),
+  formAutorization: document.querySelector('.form-authorization'),
+  formRegistration: document.querySelector('.form-register'),
+  scrollEl: document.querySelector('.js-scroll'),
 };
