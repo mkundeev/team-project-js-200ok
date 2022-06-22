6643a53fc218b4c9bd3b246eee4f272eba1a8d72
Iuliia <iuliiaiavorskaia@gmail.com>
2022-06-22 10:20:32 +0300

    add fix

--- src/js/day-night/day-night.js
+++ src/js/day-night/day-night.js
@@ -1,7 +1,7 @@
 import { refs } from '../service/refs';
-const { toggle } = refs;
+const { toggle, movieTitle } = refs;
 
-const initialState = localStorage.getItem('toggleState') == 'true';
+const initialState = localStorage.getItem('toggleState') === 'true';
 toggle.checked = initialState;
 
 toggle.addEventListener('change', addNightTheme);
@@ -11,13 +11,13 @@ function addNightTheme(evt) {
   localStorage.setItem('toggleState', toggle.checked);
   const settings = JSON.parse(localStorage.getItem('toggleState'));
 
-  if (settings) {
+  if (settings || settings) {
     document.body.classList.add('night-theme');
-
+    // movieTitle.body.classList.add('wite-title');
     // console.log(settings);
   } else {
     document.body.classList.remove('night-theme');
-
+    // movieTitle.body.classList.remove('wite-title');
     // console.log(settings);
   }
 }
